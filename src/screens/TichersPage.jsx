2ee53e9e1f92bcc7124274138afa2c71921c0d74
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2024-12-11 15:35:06 +0500

    seo

--- src/screens/TichersPage.jsx
+++ src/screens/TichersPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Classes from "../components/Classes";
+import { Helmet } from "react-helmet";
 
 const TichersPage = () => {
   return (
     <div className={`w-full pt-[140px] sm:pt-[110px] mb-10`}>
+      <Helmet>
+        <title>Tichers</title>
+        <meta name="description" content="Bu mening ajoyib React saytim" />
+        <meta
+          name="keywords"
+          content="React, SEO, web development, tichers, ticher, samarkand, sam maktab, maktab 59, 59, samarqanda maktab 59, samarqand, registon"
+        />
+      </Helmet>
       <Classes />
     </div>
   );
