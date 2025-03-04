be5a1e0ef7c25459cdabb2c1e49e0caf49c8eef8
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2025-03-04 08:31:21 +0500

    endi tayyor

--- src/screens/StudentPanel.jsx
+++ src/screens/StudentPanel.jsx
@@ -1,28 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Footer, Navbar } from "../components";
 import { styles } from "../util/styles";
-import { Outlet, useNavigate } from "react-router";
+import { Outlet } from "react-router";
 
 const StudentPanel = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    window.history.pushState(null, null, window.location.href);
-    const handlePopState = () => {
-      if (sessionStorage.getItem("studentData")) {
-        window.history.pushState(null, null, window.location.href);
-      } else {
-        navigate("/");
-      }
-    };
-
-    window.addEventListener("popstate", handlePopState);
-
-    return () => {
-      window.removeEventListener("popstate", handlePopState);
-    };
-  }, [navigate]);
-
   return (
     <>
       <Navbar />
--- src/screens/StudentPanelPage.jsx
+++ src/screens/StudentPanelPage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ResultStudent, Subjects } from "../components";
+import { useNavigate } from "react-router";
 
 const StudentPanelPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.history.pushState(null, null, window.location.href);
+    const handlePopState = () => {
+      if (sessionStorage.getItem("studentData")) {
+        window.history.pushState(null, null, window.location.href);
+      } else {
+        navigate("/");
+      }
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [navigate]);
   return (
     <>
       <Subjects />
